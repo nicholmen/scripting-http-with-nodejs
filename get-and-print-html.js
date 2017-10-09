e34f68c60be2980c1e48e1cb3781c3b7ccba66b0
Thomas Nicholson <nicholmen@gmail.com>
2017-10-09 19:37:06 +0000

    craeted getAndPrintHTML function to append chunks of data to a variable

---
+++ get-and-print-html.js
@@ -0,0 +1,31 @@
+// ultimate objective is to write an HTTPS client that reads and prints HTML from a specified host and path.
+// invoke (call) the function you wrote.
+
+var https = require('https');    
+
+// This function should use a buffering technique to append each chunk of data to a variable as it is received, and then console.log the data once all of the data has been received.
+function getAndPrintHTML () {
+  
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+  
+    var body = ''
+    
+    //http response callback
+    https.get(requestOptions, function (response) {
+      // use utf-8 encoding
+      response.setEncoding('utf8');
+    
+      // on reception of data, append each chunk to a variable
+      response.on('data', function(data) {
+        body += data
+      });
+
+      response.on('end', function() {
+        console.log(body);
+      });
+    });
+  }
+getAndPrintHTML();
