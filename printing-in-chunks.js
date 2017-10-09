2108f0fa8499a566b6369e6388bc78dbaef55acd
Thomas Nicholson <nicholmen@gmail.com>
2017-10-09 19:14:31 +0000

    changes to fix printing-in-chunks to actually print

--- printing-in-chunks.js
+++ printing-in-chunks.js
@@ -1,7 +1,4 @@
 // ultimate objective is to write an HTTPS client that reads and prints HTML from a specified host and path.
-
-
-
 // invoke (call) the function you wrote.
 
 var https = require('https');    
@@ -15,11 +12,21 @@ function getAndPrintHTMLChunks () {
   };
 
   // this function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
-  https.get(getAndPrintHTMLChunks, function (response) {
+  https.get(requestOptions, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+    
     response.on('data', function (data) {
+      // the callback is invoked when a `data` chunk  is received
       console.log('chunk received: + \n' + data + '\n');
-    })
+    });
 
-  })
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
 
+  });
 }
+getAndPrintHTMLChunks();
